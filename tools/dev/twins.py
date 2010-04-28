cd2bf2702c07bde31ecb72cf983c3ec44d723c7a
gstein <gstein@6cf1fcd0-9011-9001-7c59-26c77a7db614>
2010-04-28 06:18:59 +0000

    Add a scripting and support files to generate a list of twin primes up to the value of 2^31. These twins "grow" at approximately 1.5 times their previous value. These values will be used for the pc_hash_t type, with its internal table sizing and growth pattern.
    * tools/dev/twins.py: new script to generate a list of twin primes
    
    * tools/dev/primes.txt: supporting list of all primes approximately less
        than 2^16. this is used in (attempting to) factor candidate twin prime
        values to determine their primality
    
    * tools/dev/primes.txt.README: denote source and intent/purpose

---
+++ tools/dev/twins.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#
+# twins.py :  find twin primes, for use by pc_hash_t
+#
+# ====================================================================
+#    Licensed to the Apache Software Foundation (ASF) under one
+#    or more contributor license agreements.  See the NOTICE file
+#    distributed with this work for additional information
+#    regarding copyright ownership.  The ASF licenses this file
+#    to you under the Apache License, Version 2.0 (the
+#    "License"); you may not use this file except in compliance
+#    with the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing,
+#    software distributed under the License is distributed on an
+#    "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+#    KIND, either express or implied.  See the License for the
+#    specific language governing permissions and limitations
+#    under the License.
+# ====================================================================
+#
+
+GROWTH = 1.5
+START = 11
+MAX = 1L << 31
+
+def main():
+  current = START
+  while True:
+    print current
+
+    current = long(current * GROWTH)
+    if current > MAX:
+      break
+    if (current & 1) == 0:
+      current += 1
+
+    while True:
+      if isprime(current) and isprime(current + 2):
+        break
+      current += 2
+
+
+def isprime(n):
+  sqrt = n ** 0.5
+  for prime in PRIMES:
+    if prime > sqrt:
+      return True
+    if n % prime == 0:
+      return False
+
+
+def load_primes():
+  global PRIMES
+  PRIMES = [int(p) for p in open('primes.txt').read().split()]
+
+
+if __name__ == '__main__':
+  load_primes()
+  main()
