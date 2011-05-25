819a16e26784592305849f4dd2980ddf9e0e1998
gstein <gstein@6cf1fcd0-9011-9001-7c59-26c77a7db614>
2011-05-25 23:56:22 +0000

    Add a simple program that performs address lookups.
    * tests/test_lookup.c: new little test proggie

---
+++ tests/test_lookup.c
@@ -0,0 +1,53 @@
+/*
+  test_lookup.c :  basic test of address lookup
+
+  ====================================================================
+    Copyright 2011 Greg Stein
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+  ====================================================================
+*/
+
+#include <string.h>
+#include <stdio.h>
+
+#include "pc_types.h"
+#include "pc_misc.h"
+#include "pc_memory.h"
+#include "pc_channel.h"
+
+
+int main(int argc, const char **argv)
+{
+    pc_context_t *ctx = pc_context_create();
+    pc_pool_t *pool = pc_pool_root(ctx);
+    pc_error_t *err;
+    pc_hash_t *addresses;
+    pc_hiter_t *hi;
+
+    err = pc_address_lookup(&addresses, argv[1], atoi(argv[2]), 0, pool);
+    if (err)
+    {
+        printf("error: [%d] %s\n", pc_error_code(err), pc_error_message(err));
+        return EXIT_FAILURE;
+    }
+
+    for (hi = pc_hiter_begin(addresses, pool);
+         hi != NULL;
+         hi = pc_hiter_next(hi))
+    {
+        printf("result: %s\n", pc_hiter_key(hi));
+    }
+
+    return EXIT_SUCCESS;
+}
